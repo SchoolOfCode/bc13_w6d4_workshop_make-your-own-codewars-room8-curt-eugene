4de75984f4594b5ab006faf915dadf04c798790e
OttCoder <101673336+Ottcoder@users.noreply.github.com>
2022-11-03 15:17:45 +0000

    Kata planned and initial tests written out

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 /* 
 👉 Write your kata here!
+Write a function that converts KG into st and lbs.
+This will be rounded to the nearest lb.
+e.g. 80KG => "12st, 8lb".
+You will need to validate that the input is a positive integer.
+The output must be formatted as follows: "8st, 6lb".
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+ export function convertWeight (kg) {
+    // Code goes here!
+}
