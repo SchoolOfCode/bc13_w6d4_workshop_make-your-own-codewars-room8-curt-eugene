6a7224f13b4b4170ac59d7eb41b383f1da1c2b30
itsevgenii <smazanovich@icloud.com>
2022-11-03 15:34:29 +0000

    Wrote the code for the kata and tested against possible scenarios

--- main.test.js
+++ main.test.js
@@ -3,13 +3,11 @@ import { expect, test } from "@jest/globals";
 //👉 Write your tests below here:
 
 test.each([
-    {a: 80, expected: "12st 8lb"},
-    {a: 101, expected: "15st, 13lb"},
-    {a: 12.5, expected: "1st, 14lb"},
-    {a: 34.6, expected: "5st, 6lb"},
-])
-    ("Is the function outputting the correct string", ({a, expected}) => {
-    const actual = convertWeight(a);
-    expect(actual).toEqual(expected);
-})
-
+  { a: 80, expected: "12st, 8lb" },
+  { a: 101, expected: "15st, 13lb" },
+  { a: 12.5, expected: "1st, 14lb" },
+  { a: 34.6, expected: "5st, 6lb" },
+])("Is the function outputting the correct string", ({ a, expected }) => {
+  const actual = convertWeight(a);
+  expect(actual).toEqual(expected);
+});
